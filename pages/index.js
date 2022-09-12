81a1a27db38976fd2318be33cccbc57fd5f4ba6f
ibrahemnaser <i.naser4040@gmail.com>
2022-09-12 23:44:51 +0200

    initiate slider

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/apple.png" />
       </Head>
       <Navbar />
+      <Slider />
     </div>
   );
 }
