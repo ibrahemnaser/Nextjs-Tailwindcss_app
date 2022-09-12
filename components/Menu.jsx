58535f1d059d19096e37efc6f691ddf0efd0e706
ibrahemnaser <i.naser4040@gmail.com>
2022-09-12 23:32:50 +0200

    Add Navbar

---
+++ components/Menu.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const Menu = ({ hide }) => {
+  return (
+    <div className={`menu-list ${hide && "scale-y-0"} lg:scale-y-100`}>
+      <div className="flex justify-center flex-1">
+        <ul className="flex flex-col lg:flex-row items-center gap-4 xl:gap-6 2xl:gap-8">
+          <li>
+            <a href="#">Mac</a>
+          </li>
+          <li>
+            <a href="#">iPad</a>
+          </li>
+          <li>
+            <a href="#">iPhone</a>
+          </li>
+          <li>
+            <a href="#">Watch</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">Accessories</a>
+          </li>
+          <li>
+            <a href="#">Offers</a>
+          </li>
+          <li className="hidden lg:block">
+            <span className="block h-[110%] border-[0.5px] translate-y-[-5%]" />
+          </li>
+          <li>
+            <a href="#">Support</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Locations</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="flex gap-4 lg:gap-2 xl:gap-4 2xl:gap-6">
+          <li>
+            <a href="#">
+              <img className="w-5" src="/assets/search-xl.png" alt="search" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img className="w-5" src="/assets/user-xl.png" alt="profile" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img className="w-5" src="/assets/bag-xl.png" alt="bag" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "./Menu";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isHidden, setIsHidden] = useState(true);
+
+  const toggleMenu = () => {
+    setIsHidden((prev) => !prev);
+  };
+  return (
+    <nav className="relative bg-black text-white p-3">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <span className="pl-2 cursor-pointer">
+            <img className="w-8 lg:w-10" src="/assets/logo-xl.png" alt="logo" />
+          </span>
+          <span className="cursor-pointer lg:hidden" onClick={toggleMenu}>
+            <img className="w-8" src="/assets/menu-xl.png" alt="toggler" />
+          </span>
+          <Menu hide={isHidden} />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
